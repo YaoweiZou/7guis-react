d7eb8e786f3324881eb6ec0167271f375ff8acee
Yaowei <yaoweipublic@icloud.com>
2024-01-15 21:27:45 +0800

    update Timer

--- src/components/guis/Timer/index.tsx
+++ src/components/guis/Timer/index.tsx
@@ -1,22 +1,42 @@
 import Button from "@/components/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Timer() {
-  const [meterValue, setMeterValue] = useState(0);
+  const [rangeValue, setRangeValue] = useState(0);
+  const [time, setTime] = useState(0);
 
-  function onRangeChange(e: React.ChangeEvent<HTMLInputElement>) {}
+  function onRangeChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setRangeValue(Number(e.target.value));
+  }
 
-  function reset() {}
+  function reset() {
+    setRangeValue(0);
+    setTime(0);
+  }
+
+  const durationSeconds = Math.floor(rangeValue / 100);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (time < durationSeconds) {
+        setTime(lastTime => lastTime + 0.01);
+      } else {
+        setTime(rangeValue / 100);
+      }
+    }, 10);
+
+    return () => clearInterval(id);
+  }, [time, rangeValue, durationSeconds]);
 
   return (
     <div className="flex min-w-80 flex-col gap-3">
       <div className="flex flex-row items-center gap-3">
         <span className="min-w-28">Elapsed Time: </span>
-        <meter className="grow" value={meterValue} />
+        <meter className="grow" value={time / durationSeconds} />
       </div>
       <div className="flex flex-row items-center gap-3">
         <span className="min-w-28"></span>
-        <span>{meterValue}s</span>
+        <span>{time.toFixed(1)}s</span>
       </div>
       <div className="flex flex-row items-center gap-3">
         <span className="min-w-28">Duration: </span>
